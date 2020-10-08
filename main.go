bd64d728b7a127ad30fe487c2832c8d0dd655d21
techentertainer <workvinayak95@gmail.com>
2020-10-08 05:39:47 +0530

    running service and put into array

--- main.go
+++ main.go
@@ -3,14 +3,48 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os/exec"
+	"strings"
 )
 
+func getLog(after, before string) ([]map[string]string, error) {
+	args := []string{"log", "--pretty=format:%ai|%ae"}
+
+	if after != "" {
+		args = append(args, "--after="+after)
+	}
+
+	if before != "" {
+		args = append(args, "--before="+before)
+	}
+
+	cmd := exec.Command("git", args...)
+
+	output, err := cmd.Output()
+
+	var items []map[string]string
+	if err != nil {
+		return items, err
+	}
+	res := strings.Split(string(output), "\n")
+	for _, val := range res {
+		c := strings.Split(val, "|")
+		m := make(map[string]string)
+		m["timestamp"] = c[0]
+		m["author"] = c[1]
+		items = append(items, m)
+	}
+	fmt.Println(items)
+
+	return items, nil
+}
+
 func main() {
-	periodicity := flag.String("p", "month", "peridocity definition to day, week, month, year")
+	//periodicity := flag.String("p", "month", "peridocity definition to day, week, month, year")
 	after := flag.String("a", "", "after date (yyyy-mm-dd hh:mm)")
 	before := flag.String("b", "", "before date (yyyy-mm-dd hh:mm)")
 
 	flag.Parse()
 
-	fmt.Println(*periodicity, *after, *before)
+	getLog(*after, *before)
 }
