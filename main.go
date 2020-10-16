d69d5fbe43f1cc02f1423c394b177637e4a8ee21
techentertainer <workvinayak95@gmail.com>
2020-10-17 01:37:24 +0530

    main go

--- main.go
+++ main.go
@@ -8,6 +8,54 @@ import (
 	"strings"
 )
 
+type Bars struct {
+	Timestamp string
+	Commits   int
+}
+
+func printData(values map[string]int) {
+	for key, value := range values {
+		block := "\u2580"
+		n := 0
+		fmt.Print(key)
+		fmt.Print(" ")
+		for n <= value {
+			n += 1
+			fmt.Print(block)
+		}
+		fmt.Println()
+	}
+}
+
+func normalize(x, xmin, xmax int) int {
+	fmt.Println(x, xmin, xmax)
+	return int(float32(x-xmin) / float32(xmax-xmin))
+}
+
+func getScore(items map[string]Bars) map[string]int {
+	intVals := []int{}
+	for _, value := range items {
+		intVals = append(intVals, value.Commits)
+	}
+	xMax := intVals[0]
+	xMin := intVals[0]
+
+	for _, val := range intVals {
+		if xMax < val {
+			xMax = val
+		}
+		if xMin > val {
+			xMax = val
+		}
+	}
+
+	dateWiseLength := make(map[string]int)
+	for key, value := range items {
+		dateWiseLength[key] = normalize(value.Commits, xMin, xMax)
+	}
+	return dateWiseLength
+}
+
 func getCommitLog(after, before string) ([]map[string]string, error) {
 	args := []string{"log", "--pretty=format:%ai|%ae"}
 	if after != "" {
@@ -35,8 +83,23 @@ func getCommitLog(after, before string) ([]map[string]string, error) {
 	return items, nil
 }
 
+func filterAsPerPeriodicity(items []map[string]string, periodicity string) map[string]Bars {
+	bars := make(map[string]Bars)
+
+	for i := 0; i < len(items); i++ {
+		timestamp := items[i]["timestamp"]
+		bar := Bars{Timestamp: timestamp, Commits: 0}
+		if _, found := bars[timestamp]; !found {
+			bar.Commits += 1
+		}
+		bars[timestamp] = bar
+	}
+
+	return bars
+}
+
 func main() {
-	//periodicity := flag.String("p", "month", "peridocity definition to day, week, month, year")
+	periodicity := flag.String("p", "month", "peridocity definition to day, week, month, year")
 	after := flag.String("a", "", "after date (yyyy-mm-dd hh:mm)")
 	before := flag.String("b", "", "before date (yyyy-mm-dd hh:mm)")
 	flag.Parse()
@@ -49,9 +112,9 @@ func main() {
 
 	if len(items) > 0 {
 		fmt.Printf("%d commits\n", len(items))
+		data := filterAsPerPeriodicity(items, *periodicity)
+		printData(getScore(data))
 	} else {
 		fmt.Println("No commits to plot")
 	}
-
-	fmt.Println(items)
 }
